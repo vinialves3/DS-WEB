779ea283cb627c994d67c331fc885c927bca9ada
vinialves3 <vinicius.af27@gmail.com>
2024-11-26 11:39:33 -0300

    Novas adições

--- AtividadeArray/ativbid.php
+++ AtividadeArray/ativbid.php
@@ -1,18 +1,33 @@
 <?php 
 
-
 /* SEGUNDA ATIVIDADE */
+    /* PRODUTOS */
+
+$produtos = array(
+    array('nomeprod' => 'PinhoSol', 'precinho' => 30000, 'estoqueprod' => 3),
+    array('nomeprod' => 'Hersheys', 'precinho' => 0.13, 'estoqueprod' => 5),
+    array('nomeprod' => 'GodzillaEnergeticos', 'precinho' => 20, 'estoqueprod' => 7),
+);
+
+foreach ($produtos as $prod) {
+    $total = $prod['precinho'] * $prod['estoqueprod'];
+    echo 'Produto: ' . $prod['nomeprod'] . ' - Total: ' . $total . '<br><br><br>';
+}
 
 
+
+/* NOTAS ALUNOS */
 $alunos = array(
-    array('nome' => 'PinhoSol', 'precinho' => 30000, 'estoque' => 3),
-    array('nome' => 'Hersheys', 'precinho' => 0.13, 'estoque' => 5),
-    array('nome' => 'GodzillaEnergeticos', 'precinho' => 20, 'estoque' => 7)
-    );[
+    array('nome' => 'Ana', 'matematica' => 8, 'portugues' => 7),
+    array('nome' => 'Bruno', 'matematica' => 6, 'portugues' => 9),
+    array('nome' => 'Carlos', 'matematica' => 7, 'portugues' => 8)
+);
 
-    
+foreach ($alunos as $aluno) {
+    $total = ($aluno['matematica'] + $aluno['portugues']) / 2;
+    echo 'Nome do aluno: ' . $aluno['nome'] . ' - Média do aluno: ' . $total . '<br>';
+}
 
-        echo 'preco' * 'estoque'
-    ]
+
 
 ?>
