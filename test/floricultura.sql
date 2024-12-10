84cded15e827f2e0ec853cc002cda9fca288f785
vinialves3 <vinicius.af27@gmail.com>
2024-12-10 12:35:17 -0300

    Adicionando arquivo sql

---
+++ test/floricultura.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE floriculturaa
+    CREATE TABLE utilitarios;
+    CREATE TABLE usuarios;
+    INSERT INTO usuarios (nome, email, senha)
+    VALUES ('admin', 'admin@admin.com', '123')
