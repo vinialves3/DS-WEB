2db720bfba51a40c26c4e1a2b06db903306c18cf
vinialves3 <vinicius.af27@gmail.com>
2025-01-31 12:18:29 -0300

    Aula 01 de JavaScript

---
+++ JavaScript/Aula-01/README.md
@@ -0,0 +1,9 @@
+Aula introdutória sobre Lógica de Programação com JavaScript
+
+Declaração de variáveis
+
+Alert, Confirm, Prompt
+
+Conversão numérica
+
+Referência: Apostila aula 1 de JavaScript
---
+++ JavaScript/Aula-01/script.js
@@ -0,0 +1,27 @@
+console.log("Olá mundo");
+
+// Exemplo de alert
+window.alert("teste");
+
+// Exemplo de confirm
+var teste = confirm ("Oi");
+
+// Exibindo a opção escolhida pelo usuário
+console.log(teste);
+
+// Exemplo de prompt
+
+var testePrompt = prompt ("Digite seu nome");
+
+// Exibindo o nome do usuário
+console.log(testePrompt);
+alert(prompt);
+
+// Tentativa de soma
+var numero1 = Number.parseInt (prompt("Digite o primeiro número"));
+var numero2 = Number.parseInt (prompt("Digite o segundo número"));
+alert(numero1 + numero2);
+
+
+console.log(typeof(numero1));
+alert(numero1 + numero2);
